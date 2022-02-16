9ec81fbf88d652b582aa44882c66f394aa571735
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-02-16 14:17:07 +0530

    changes added

--- canefatti_anim.js
+++ canefatti_anim.js
@@ -21,12 +21,14 @@ function play_animation() {
 
 let text_container = document.querySelector(".text")
 let card_container = document.querySelector(".card")
+let wish_gallery_container = document.querySelector(".wish_gallery")
 
-let animation = false // set it to true for oning animation
+let animation = true // set it to true for oning animation
 
 if (animation){
     text_container.style.display = "none"
     card_container.style.display = "none"
+    wish_gallery_container.style.display = "none"
     
     let envelope = document.querySelector(".box");
     envelope.onclick = ()=>{
@@ -40,5 +42,6 @@ if (animation){
     
         text_container.style.display="flex"
         card_container.style.display="flex"
+        wish_gallery_container.style.display="flex"
     }
 }
